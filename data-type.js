48a12040d2846412f40028c9d69faa3e978337dd
akilinjamam <akilinjamam@gmail.comm>
2022-02-25 11:20:16 +0600

    file is adding

---
+++ data-type.js
@@ -0,0 +1,11 @@
+//  javascript data type
+
+// js identify what is the type of the variable , depends on the value which exists in the right side of the variable. to understand the type of the variable, use typeOf.
+
+// const a = 14;
+
+// const a = 'jamal uddin babu'
+
+
+const a = true
+console.log(typeof a);
