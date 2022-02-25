d1a2ad26313e89c8fd7eec3da9290f27ea07f07b
akilinjamam <akilinjamam@gmail.comm>
2022-02-25 21:49:07 +0600

    file is added too.

---
+++ true-false.js
@@ -0,0 +1,20 @@
+// 0 is by defult a negetive value
+
+//  '' empty string is by defult negetive value. even if you give  a space under a string . it will be counted as a positive value.
+
+// NaN means not a number 
+
+// undefined means any variable which is not been decleared. this is one kind of negetive value too.
+// null. this is also a negetive value
+
+
+let x = ' ';
+
+if (x) {
+    console.log('truethy')
+}
+
+else {
+    console.log('falsy')
+}
+
